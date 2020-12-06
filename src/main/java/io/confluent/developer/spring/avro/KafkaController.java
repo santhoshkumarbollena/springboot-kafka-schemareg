bf34659aa8eabae2f3448ea0089decd09aed021c
Santhosh <bollenasanhosh@gmail.com>
2020-12-06 13:24:46 -0600

    Added the array for email

---
+++ src/main/java/io/confluent/developer/spring/avro/KafkaController.java
@@ -0,0 +1,30 @@
+package io.confluent.developer.spring.avro;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.confluent.developer.User;
+import java.util.*;
+
+@RestController
+@RequestMapping(value = "/user")
+public class KafkaController {
+
+  private final Producer producer;
+
+  @Autowired
+  KafkaController(Producer producer) {
+    this.producer = producer;
+  }
+
+  @PostMapping(value = "/publish")
+  public void sendMessageToKafkaTopic(@RequestParam("name") String name, @RequestParam("age") Integer age) {
+	  ArrayList<String> ai=new ArrayList<>();
+	  ai.add("asda@g");
+	  ai.add("asdas@y");
+    this.producer.sendMessage(new User(name, age,ai));
+  }
+}
